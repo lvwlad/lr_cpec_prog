3e21a7d297ed08d125e4cac8a3e6f23d376fb3d3
lvwlad <vlad04564@gmail.com>
2025-05-12 12:00:47 +0300

    Create mainwindow.cpp

---
+++ lr5/mainwindow.cpp
@@ -0,0 +1,142 @@
+#include "mainwindow.h"
+#include <QUndoStack>
+#include <QAction>
+#include <QKeySequence>
+
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+    model = new GraphicModel(this);
+    undoStack  = new QUndoStack(this);
+    undoAction = new QAction(tr("Undo"), this);
+    undoAction->setShortcut(QKeySequence::Undo);
+    undoAction->setEnabled(false);
+    connect(undoAction, &QAction::triggered,  undoStack, &QUndoStack::undo);
+    connect(undoStack,  &QUndoStack::canUndoChanged, undoAction, &QAction::setEnabled);
+    redoAction = new QAction(tr("Redo"), this);
+    redoAction->setShortcut(QKeySequence::Redo);
+    redoAction->setEnabled(false);
+    connect(redoAction, &QAction::triggered,  undoStack, &QUndoStack::redo);
+    connect(undoStack,  &QUndoStack::canRedoChanged, redoAction, &QAction::setEnabled);
+    controller = new GraphicController(model, undoStack, this);
+
+    setupUI();
+    setupToolBar();
+    setupConnections();
+}
+
+MainWindow::~MainWindow() {}
+
+void MainWindow::setupUI() {
+    view = new QGraphicsView(this);
+    view->setScene(model->getScene());
+    view->setRenderHint(QPainter::Antialiasing);
+    view->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+    view->setDragMode(QGraphicsView::RubberBandDrag);
+    view->setInteractive(true);
+    setCentralWidget(view);
+
+    toolBar = new QToolBar("Tools", this);
+    addToolBar(Qt::LeftToolBarArea, toolBar);
+}
+
+void MainWindow::onTriangleAction() {
+    controller->setEditorMode(EditorMode::CreateTriangle);
+    view->setDragMode(QGraphicsView::NoDrag);
+}
+
+void MainWindow::setupToolBar() {
+    QAction* selectAction = toolBar->addAction("Select");
+    QAction* lineAction = toolBar->addAction("Line");
+    QAction* rectAction = toolBar->addAction("Rectangle");
+    QAction* ellipseAction = toolBar->addAction("Ellipse");
+    QAction* triangleAction = toolBar->addAction("Triangle");
+    connect(triangleAction, &QAction::triggered, this, &MainWindow::onTriangleAction);
+    QAction* textAction = toolBar->addAction("Text");
+    toolBar->addSeparator();
+    QAction* colorAction = toolBar->addAction("Color");
+    connect(colorAction, &QAction::triggered, this, &MainWindow::onColorAction);
+    toolBar->addSeparator();
+    QAction* deleteAction = toolBar->addAction("Delete");
+    QAction* clearAction = toolBar->addAction("Clear");
+    toolBar->addSeparator();
+    toolBar->addAction(undoAction);
+    toolBar->addAction(redoAction);
+
+    connect(selectAction, &QAction::triggered, this, &MainWindow::onSelectAction);
+    connect(lineAction, &QAction::triggered, this, &MainWindow::onLineAction);
+    connect(rectAction, &QAction::triggered, this, &MainWindow::onRectAction);
+    connect(ellipseAction, &QAction::triggered, this, &MainWindow::onEllipseAction);
+    connect(textAction, &QAction::triggered, this, &MainWindow::onTextAction);
+    connect(colorAction, &QAction::triggered, this, &MainWindow::onColorAction);
+    connect(deleteAction, &QAction::triggered, this, &MainWindow::onDeleteAction);
+    connect(clearAction, &QAction::triggered, this, &MainWindow::onClearAction);
+}
+
+void MainWindow::setupConnections() {
+    connect(model->getScene(), &CustomGraphicsScene::sceneMousePressed,
+            this, &MainWindow::handleMousePressed);
+    connect(model->getScene(), &CustomGraphicsScene::sceneMouseMoved,
+            this, &MainWindow::handleMouseMoved);
+    connect(model->getScene(), &CustomGraphicsScene::sceneMouseReleased,
+            this, &MainWindow::handleMouseReleased);
+}
+
+void MainWindow::onSelectAction() {
+    controller->setEditorMode(EditorMode::Select);
+    view->setDragMode(QGraphicsView::RubberBandDrag);
+}
+
+void MainWindow::onLineAction() {
+    controller->setEditorMode(EditorMode::CreateLine);
+    view->setDragMode(QGraphicsView::NoDrag);
+}
+
+void MainWindow::onRectAction() {
+    controller->setEditorMode(EditorMode::CreateRect);
+    view->setDragMode(QGraphicsView::NoDrag);
+}
+
+void MainWindow::onEllipseAction() {
+    controller->setEditorMode(EditorMode::CreateEllipse);
+    view->setDragMode(QGraphicsView::NoDrag);
+}
+
+void MainWindow::onTextAction() {
+    controller->setEditorMode(EditorMode::CreateText);
+    view->setDragMode(QGraphicsView::NoDrag);
+}
+
+void MainWindow::onColorAction() {
+    QColor color = QColorDialog::getColor(controller->getCurrentColor(),
+                                        this, "Select Color");
+    if (color.isValid()) {
+        controller->setCurrentColor(color);
+        controller->changeSelectedItemsColor(color);
+    }
+}
+
+void MainWindow::onDeleteAction() {
+    controller->deleteSelectedItems();
+}
+
+void MainWindow::onClearAction() {
+    controller->clearAll();
+}
+
+void MainWindow::keyPressEvent(QKeyEvent* event) {
+    if (event->key() == Qt::Key_Delete) {
+        controller->deleteSelectedItems();
+    }
+    QMainWindow::keyPressEvent(event);
+}
+
+void MainWindow::handleMousePressed(const QPointF& pos) {
+    controller->mousePressed(pos);
+}
+
+void MainWindow::handleMouseMoved(const QPointF& pos) {
+    controller->mouseMoved(pos);
+}
+
+void MainWindow::handleMouseReleased() {
+    controller->mouseReleased();
+}
