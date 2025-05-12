6822e3f5eb80da92360f2f2bfff9ead1336b4a12
lvwlad <vlad04564@gmail.com>
2025-05-12 11:59:36 +0300

    Create mainwindow.h

---
+++ lr5/mainwindow.h
@@ -0,0 +1,55 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsView>
+#include <QToolBar>
+#include <QColorDialog>
+#include <QInputDialog>
+#include <QKeyEvent>
+#include "graphicmodel.h"
+#include "graphiccontroller.h"
+#include <QUndoStack>
+#include <QAction>
+#include <QKeySequence>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+protected:
+    void keyPressEvent(QKeyEvent* event) override;
+
+private slots:
+    void onSelectAction();
+    void onLineAction();
+    void onRectAction();
+    void onEllipseAction();
+    void onTextAction();
+    void onTriangleAction();
+    void onColorAction();
+    void onDeleteAction();
+    void onClearAction();
+
+    void handleMousePressed(const QPointF& pos);
+    void handleMouseMoved(const QPointF& pos);
+    void handleMouseReleased();
+
+private:
+    void setupUI();
+    void setupToolBar();
+    void setupConnections();
+
+    QGraphicsView* view;
+    QToolBar* toolBar;
+
+    GraphicModel* model;
+    GraphicController* controller;
+    QUndoStack* undoStack;
+    QAction* undoAction;
+    QAction* redoAction;
+};
+
+#endif // MAINWINDOW_H
