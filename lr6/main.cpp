306577fed4f7b61e8e71f7c72815c003cb12aae0
nvrstpx <kosinov.artem.2004@gmail.com>
2025-05-13 18:23:45 +0300

    Update main.cpp

--- lr6/main.cpp
+++ lr6/main.cpp
@@ -1,1 +1,24 @@
-hdhdh
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QTranslator>
+#include <QLocale>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("MyCompany");
+    QCoreApplication::setApplicationName("WeatherApp");
+
+    QTranslator translator;
+    if (translator.load(QLocale(), "weatherapp", "_", ":/i18n")) {
+        app.installTranslator(&translator);
+    }
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("WeatherApp/Main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
